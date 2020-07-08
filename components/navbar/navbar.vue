86e88358c4c4229bfd89dff22291eebc03527cbd
jiucheng <15279232381@163.com>
2020-07-08 22:43:49 +0800

    首页顶部导航栏及适配功能完成

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,100 @@
+<template>
+	<view class="navbar">
+		<view class="navbar-fixed">
+			<!-- 状态栏 -->
+			<view :style="{ height: statusBarHeight + 'px' }"></view>
+			<!-- 导航栏内容 -->
+			<view class="navbar-content" :style="{ height: navBarHeight + 'px', width: windowWidth + 'px'}">
+				<view class="navbar-search">
+					<view class="navbar-search_icon">
+						<!-- <uni-icons type="search"></uni-icons> -->
+						<text class="iconfont icon-search"></text>
+					</view>
+					<view class="navbar-search_text">uniapp、vue</view>
+				</view>
+			</view>
+		</view>
+		<view style="height: 45px;"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				statusBarHeight: 20,
+				navBarHeight: 45,
+				windowWidth: 375
+			}
+		},
+		methods: {
+			
+		},
+		created() {
+			// 获取手机系统信息
+			const info = uni.getSystemInfoSync()
+			// 设置状态栏高度
+			this.statusBarHeight = info.statusBarHeight
+			// 屏幕本来的位置
+			this.windowWidth = info.windowWidth
+			// console.log(info)
+			
+			// h5 app mp-alipay 这三个平台不支持这个方法
+			// #ifdef H5 || APP-PLUS || MP-ALIPAY
+			// 获取胶囊的位置
+			const menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+			console.log(menuButtonInfo)
+			// (胶囊底部高度 - 状态栏的高度) + (胶囊顶部高度 - 状态栏内的高度) = 导航栏的高度
+			this.navBarHeight = (menuButtonInfo.bottom - info.statusBarHeight) + (menuButtonInfo.top - info.statusBarHeight)
+			this.windowWidth = menuButtonInfo.left
+			// console.log(navBarHeight)
+			// #endif
+		}
+	}
+</script>
+
+<style lang="less">
+	@import '../../common/css/icon.css';
+	
+	.navbar {
+		.navbar-fixed {
+			position: fixed;
+			top: 0;
+			left: 0;
+			z-index: 99;
+			width: 100%;
+			background-color: #f07373;
+			.navbar-content {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				padding: 0 15px;
+				height: 45px;
+				box-sizing: border-box;
+				.navbar-search {
+					display: flex;
+					align-items: center;
+					padding: 0 10px;
+					width: 100%;
+					height: 30px;
+					border-radius: 30px;
+					background-color: #FFFFFF;
+					.navbar-search_icon {
+						// width: 10px;
+						// height: 10px;
+						// border: 1px solid red;
+						margin-right: 10px;
+						.icon-search {
+							font-size: 16;
+							color: #999;
+						}
+					}
+					.navbar-search_text {
+						font-size: 12px;
+						color: #999;
+					}
+				}
+			}
+		}
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,15 +1,23 @@
 <template>
 	<view class="content">
-		首页
+		<!-- 自定义导航栏 -->
+		<NavBar></NavBar>
+		<view v-for="(item,index) in 100" :key="index">{{ item }}</view>
 	</view>
 </template>
 
 <script>
+	// easyCom components/组件名/组件名.vue 局部引入
+	import NavBar from '@/components/navbar/navbar';
+	
 	export default {
 		data() {
 			return {
 				title: 'Hello'
 			}
+		},
+		components:{
+			NavBar
 		},
 		onLoad() {
 
